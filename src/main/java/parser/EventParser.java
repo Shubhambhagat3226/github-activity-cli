0fd52ccffa628c4cfa470aebbf6f336411973b5b
Shubham Kumar <shubhambhagat3220@gmail.com>
2026-03-07 15:53:46 +0000

    Implement JSON parsing using Jackson

---
+++ src/main/java/parser/EventParser.java
@@ -0,0 +1,13 @@
+package parser;
+
+import com.fasterxml.jackson.databind.ObjectMapper;;
+import models.Event;
+
+public class EventParser {
+
+    public static Event[] parse(String json) throws Exception {
+
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(json, Event[].class);
+    }
+}
