c854b23ac33315f3940c1d0c6b2f75a29617e656
Shubham Kumar <shubhambhagat3220@gmail.com>
2026-03-20 15:04:08 +0000

    feat(cli): add CLI arguments model

---
+++ src/main/java/com/githubactivity/cli/args/CliArguments.java
@@ -0,0 +1,51 @@
+package com.githubactivity.cli.args;
+
+public class CliArguments  {
+    private String username;
+    private String type;
+    private Integer limit;
+    
+
+    /**
+     * @return String return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /**
+     * @return String return the type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * @return Integer return the limit
+     */
+    public Integer getLimit() {
+        return limit;
+    }
+
+    /**
+     * @param limit the limit to set
+     */
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
+
+}
